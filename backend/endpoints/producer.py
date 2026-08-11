052e2a36ba56db62947904aa6572f8cd969758dc
DemiGod108 <rkkaranth2010@gmail.com>
2026-08-11 17:08:55 +0530

    feat: setup kafka producer >had to correctly configure KAFKA_ADVERTISED_LISTENERS, so that client can connect and actually produce kafka records/event

--- backend/endpoints/producer.py
+++ backend/endpoints/producer.py
@@ -1,4 +1,20 @@
 from fastapi import APIRouter, Request
+from confluent_kafka import Producer
+import json
+
+producer_config= {
+	"bootstrap.servers": "kafka:9092"
+}
+
+producer = Producer(producer_config)
+
+def ack(error, msg):
+	if msg:
+		print(f"Topic name: {msg.topic()}")
+		print(f"Partition: {msg.partition()}")
+		print(f"Payload: {msg.value().decode('utf-8')}")
+	else:
+		print(error)
 
 router = APIRouter()
 
@@ -6,6 +22,18 @@ router = APIRouter()
 async def webhook_payload(request: Request):
 	payload = await request.json()
 	repo_id = payload["repository"]["id"]
+	repo_name = payload["repository"]["name"]
 	event = request.headers.get("X-Github-Event")
 
-	print(repo_id, event)
+	kafka_record = json.dumps(payload).encode('utf-8')
+
+	print("route hit")
+
+	producer.produce(
+		topic=f"{repo_id}-{repo_name}",
+		headers={"event": event},
+		value=kafka_record,
+		callback=ack
+	)
+
+	producer.poll(0)
