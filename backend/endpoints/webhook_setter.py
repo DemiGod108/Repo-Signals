313d30cce81b1f5baa7ab3eae39f551095aadb18
DemiGod108 <rkkaranth2010@gmail.com>
2026-08-17 22:51:06 +0530

    chore: cleaned up the setup-webhook endpoint, eliminated hihgly nested code >added a utility function that gives a db session

--- backend/endpoints/webhook_setter.py
+++ backend/endpoints/webhook_setter.py
@@ -6,20 +6,14 @@ from schemas import SelectRepo
 from utils.auth import get_current_user
 from utils.encrypt_decrypt import cipher
 from utils.config import backend_ngrok
-from database import sessionLocal
+from utils.db_session import get_db
+from sqlalchemy.orm import Session
 from models import TrackedRepo
-
-def get_db():
-	db = sessionLocal()
-	try:
-		yield db
-	finally:
-		db.close()
 
 router = APIRouter()
 
 @router.post("/setup-webhook")
-async def setup_webhooks(repo_name: SelectRepo, user: Users = Depends(get_current_user), db: TrackedRepo = Depends(get_db)):
+async def setup_webhooks(repo_name: SelectRepo, user: Users = Depends(get_current_user), db: Session = Depends(get_db)):
 	encrypted_github_token = user.encrypted_github_access_token.encode('utf-8') #in db it will stored in string format, but for .decrypt() to work it needs encrypted content in bytes format hence encoding it
 	decrypted_github_token = cipher.decrypt(encrypted_github_token).decode('utf-8') #needs to be in string format for Authorization Bearer to work
 
@@ -30,86 +24,77 @@ async def setup_webhooks(repo_name: SelectRepo, user: Users = Depends(get_curren
 
 	#we need to retrieve repo details in order to decide whether this current user can setup webhook for the specified repo or not
 	async with httpx.AsyncClient() as client:
-		repo_details = await client.get(url=f"https://api.github.com/repos/{user.github_username}/{repo_name.repo_name}", headers=headers)
-	resp = repo_details.json()
+		repo_details_req = await client.get(url=f"https://api.github.com/repos/{user.github_username}/{repo_name.repo_name}", headers=headers)
+	repo_details = repo_details_req.json()
 
-	repository_id = resp["id"]
-	repository_name = resp["name"]
+	#handles the case when the user entered repo doesnt even exist, if it doesnt exists exit the function
+	if repo_details_req.status_code == httpx.codes.NOT_FOUND:
+		raise HTTPException(detail="Repo not found", status_code=status.HTTP_404_NOT_FOUND)
+
+	#this if branch is for handling case when the github access token was manually revoked by the user in the github website, if token is invalid again exit the function right away
+	if repo_details_req.status_code != 200:
+		raise HTTPException(detail="failed to fetch repo details", status_code=repo_details_req.status_code)
+
+	#repo exists, github access token is valid hence we extract repo info (repo_details_req.status_code==200 case:)
+	repository_id = repo_details["id"]
+	repository_name = repo_details["name"]
+
+	#if the current user isnt the admin of the repo then they cant setup webhook (as per github only admins can setup webhooks for a particular repo) hence just exit the function right away
+	if not repo_details["permissions"]["admin"]:
+		raise HTTPException(detail="you are not the admin", status_code=status.HTTP_403_FORBIDDEN)
+
+	#if the execution reached here that means, repo exists, current user is indeed its admin 
 	tracked_repo_obj = db.query(TrackedRepo).filter(TrackedRepo.user_id == user.github_id, TrackedRepo.repo_id == repository_id).first()
 
-	#this handles the case when the admin (i.e the repo maintainer) who setup webhook is trying to setup webhook for the same repo again (same maintainer trying to setup webhook for the same repo, hence repo_id, github_userid pair isnt unique)
+	#this handles the case when the admin (i.e the repo maintainer) who setup webhook is trying to setup webhook for the same repo again (same maintainer trying to setup webhook for the same repo, hence repo_id, github_userid pair isnt unique), exit right away
 	if tracked_repo_obj:
-		raise HTTPException(detail="repository is already added")
+		raise HTTPException(detail="repository is already added", status_code=status.HTTP_400_BAD_REQUEST)
+
+	data = {
+		"name": "web",
+		"config": {
+			"url": f"{backend_ngrok}/webhook-payload",
+			"content_type": "json",
+			"insecure_ssl": "0"
+		},
+		"events": ["push", "pull_request", "pull_request_review", "fork", "issues", "issue_comment"]
+	}
+	
+	#setting up webhooks
+	async with httpx.AsyncClient() as client:
+		webhook = await client.post(f"https://api.github.com/repos/{user.github_username}/{repo_name.repo_name}/hooks", headers=headers, json=data)
+
+	#happy path, webhook gets setup and we need to insert the repo details and current user's github id into db
+	if webhook.status_code == httpx.codes.CREATED:
+		db.add(TrackedRepo(
+			repo_id=repository_id,
+			repo_name=repository_name,
+			user_id=user.github_id
+		))
+		db.commit()
+		return JSONResponse(
+			content={"detail": "webhook created"},
+			status_code=status.HTTP_201_CREATED
+		)
+	
+	#this elif branch is meant for handling case when a repo has two (or more) maintainers and the webhook for that repo was already created by some other maintainer, but we are inserting into db table cause (repo_id, github_useid) pair is unique (two different repo maintainer of the same repo will obviously have diff github_userid)
+	elif webhook.status_code == httpx.codes.UNPROCESSABLE_ENTITY:
+		error_body = webhook.json()
+		error_msg = error_body.get("errors")[0].get("message")
+		
+		if error_msg == "Hook already exists on this repository":
+			db.add(TrackedRepo(
+				repo_id=repository_id,
+				repo_name=repository_name,
+				user_id=user.github_id
+			))
+			db.commit()
+			return JSONResponse(
+				content={"detail": "webhook created"},
+				status_code=status.HTTP_201_CREATED
+			)
+		else:
+			raise HTTPException(status_code=webhook.status_code, detail="failed to create webhook")
 
 	else:
-		#if repo exists and the current user is the admin, setup the webhook (as per github, only admins can setup webhooks for a repo)
-		if repo_details.status_code == httpx.codes.OK and resp["permissions"]["admin"]:
-			headers={
-				"accept": "application/vnd.github+json",
-				"Authorization": f"Bearer {decrypted_github_token}"
-			}
-			data = {
-				"name": "web",
-				"config": {
-					"url": f"{backend_ngrok}/webhook-payload",
-					"content_type": "json",
-					"insecure_ssl": "0"
-				},
-				"events": ["push", "pull_request", "pull_request_review", "fork", "issues", "issue_comment"]
-			}
-			#setting up webhooks
-			async with httpx.AsyncClient() as client:
-				webhook = await client.post(f"https://api.github.com/repos/{user.github_username}/{repo_name.repo_name}/hooks", headers=headers, json=data)
-
-
-			#happy path, webhook gets setup and we need to insert the repo details and current user's github id into db
-			if webhook.status_code == httpx.codes.CREATED:
-				db.add(TrackedRepo(
-					repo_id=repository_id,
-					repo_name=repository_name,
-					user_id=user.github_id
-				))
-				db.commit()
-
-				return JSONResponse(
-					content={"detail": "webhook created"},
-					status_code=status.HTTP_201_CREATED
-				)
-
-			#this elif branch is meant for handling case when a repo has two (or more) maintainers and the webhook for that repo was already created by some other maintainer, but we are inserting into db table cause (repo_id, github_useid) pair is unique (two different repo maintainer of the same repo will obviously have diff github_userid)
-			elif webhook.status_code == httpx.codes.UNPROCESSABLE_ENTITY:
-				error_body = webhook.json()
-				error_msg = error_body.get("errors")[0].get("message")
-
-				if error_msg == "Hook already exists on this repository":
-					db.add(TrackedRepo(
-						repo_id=repository_id,
-						repo_name=repository_name,
-						user_id=user.github_id
-					))
-					db.commit()
-					return JSONResponse(
-						content={"detail": "webhook created"},
-						status_code=status.HTTP_201_CREATED
-					)
-				else:
-					raise HTTPException(status_code=webhook.status_code, detail="failed to create webhook")
-		
-			#sad path
-			else:
-				data = webhook.json()
-				print(data["errors"])
-				raise HTTPException(status_code=webhook.status_code, detail="failed to create webhook")
-
-
-		#repo exists but the current user is not the admin (user is not admin, hence forbidden from setting up webhook)
-		elif repo_details.status_code == httpx.codes.OK:
-			raise HTTPException(detail="repo exists but you are not the admin", status_code=status.HTTP_403_FORBIDDEN)
-
-		#the repo which the user entered doesnt even exist
-		elif repo_details.status_code == httpx.codes.NOT_FOUND:
-			raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="repository does not exist")
-
-		#occurs if, the github access token is invalid, or revoked (this happens when user manually revokes the app)
-		else:
-			raise HTTPException(status_code=repo_details.status_code, detail="failed to fetch repo details")
+		raise HTTPException(status_code=webhook.status_code, detail="failed to create webhook")
