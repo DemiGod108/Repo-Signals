2cfd9e2e8a8c3f618ecf260aafc5447990f45739
DemiGod108 <rkkaranth2010@gmail.com>
2026-09-13 11:41:18 +0530

    feat: backend endpoint added for chart generation for metric-1

--- backend/endpoints/overview.py
+++ backend/endpoints/overview.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 from sqlalchemy.dialects.postgresql import JSONB
+from datetime import datetime, timedelta, UTC
 from models import Users, EventData, TrackedRepo, ActiveRepoItems
 from database import get_db
 from utils.auth import get_current_user
@@ -43,3 +45,55 @@ def repo_overview(repo_id: int, user: Users = Depends(get_current_user), db: Ses
 		content={"repo_overview": overview},
 		status_code=status.HTTP_200_OK
 	)
+
+
+@router.get("/graph/{repo_id}")
+def activity_graph(repo_id: int, user: Users = Depends(get_current_user), db: Session = Depends(get_db)):
+	github_id = user.github_id
+
+	tracked_repo_obj = db.query(TrackedRepo).filter(TrackedRepo.user_id == github_id, TrackedRepo.repo_id == repo_id).first()
+
+	if not tracked_repo_obj:
+		raise HTTPException(detail="invalid repository", status_code=status.HTTP_400_BAD_REQUEST)
+	
+	today = datetime.now(tz=UTC).date() #this is a date object NOT datetime object anymore because of .date
+	past = today - timedelta(days=29) #past is of type date, because we subtracted number of days from date object (i.e today)
+
+	#in tracked_repo table the tracking_started_at is stored as timezone aware object, i.e it is a datetime object so we need to convert even tracking_started_at to date object so that we can subtract it from variable today with no TypeErrors
+	tracking_started_at = tracked_repo_obj.tracking_started_at.date()
+
+	#same idea as spike / delcine metric
+	if (today - tracking_started_at).days < 14:
+		raise HTTPException(detail="insufficient data", status_code=status.HTTP_404_NOT_FOUND)
+
+	#the db column: event_occured_at stores values that are of type datetime in python, so i cant directly compare it with today and past cause both or date objects, so before comparing i need to convert event_occured_at from datetime object to date object which is done by using func.date().
+	event_objs = db.query(EventData).filter(func.date(EventData.event_occurred_at) <= today, func.date(EventData.event_occurred_at) >= past, EventData.repo_id == repo_id).order_by(func.date(EventData.event_occurred_at)).all()
+
+	date_count = {}
+	#loop stores number of events occured on each calendar date on that window (today-past window)
+	for event_obj in event_objs:
+		event_date = event_obj.event_occurred_at.date()
+		date_count[event_date] = date_count.get(event_date, 0) + 1
+
+	#this loop just fills the date_count dictionary with missing dates, and sets its value to zero, so that chart.js and render graphs properly
+	running_date = past
+	while running_date <= today:
+		if running_date not in date_count:
+			date_count[running_date] = 0
+
+		running_date += timedelta(days=1)
+
+	#sorting in chronological order, so that graphs can be render properly
+	#sorted() on a dictionary returns a list of tuples something like: [(key1, value1), (key2, value2), ...] but in sorted order, sorting happens based on dates, because its the first item in the tuple of (date, count)
+	sorted_date_count = sorted(date_count.items()) 
+
+	#building the response:
+	dates_list=[]
+	counts_list=[]
+	for date, count in sorted_date_count:
+		dates_list.append(date)
+		counts_list.append(count)
+
+	json_safe_dates_list = jsonable_encoder(dates_list) #datetime object arent json safe so need to convert them, whereas play integer stored in counts is json safe
+
+	return JSONResponse(content={'dates': json_safe_dates_list, 'count': counts_list}, status_code=status.HTTP_200_OK)
